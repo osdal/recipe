944d91600cb64de4bd24291c5397095eb0e0b8ff
osdal <osdal67@gmail.com>
2019-05-31 13:33:15 +0300

    Просмотр рецепта

--- src/app/resipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/resipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import {Recipe} from '../../recipe.model'
+// import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,10 +10,15 @@ import {Recipe} from '../../recipe.model'
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelected() {
+    this.recipeSelected.emit();
   }
 
 }
